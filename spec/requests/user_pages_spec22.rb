0edec04f157269ef755960b8557f95cadd151849
AK <ak_pdx@yahoo.com>
2012-07-11 18:44:31 -0700

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec22.rb
+++ spec/requests/user_pages_spec22.rb
@@ -4,7 +4,23 @@ describe "User pages" do
 
   subject { page }
 
-describe "index" do
+  describe "index" do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+      visit users_path
+    end
+
+    it { should have_selector('title', text: 'All users') }
+    it { should have_selector('h1',    text: 'All users') }
+
+    it "should list each user" do
+      User.all.each do |user|
+        page.should have_selector('li', text: user.name)
+      end
+    end
+  end
 
     let(:user) { FactoryGirl.create(:user) }
 
@@ -29,44 +45,7 @@ describe "index" do
         end
       end
     end
-    
-    describe "delete links" do
-
-      it { should_not have_link('delete') }
-
-      describe "as an admin user" do
-        let(:admin) { FactoryGirl.create(:admin) }
-        before do
-          sign_in admin
-          visit users_path
-        end
-
-        it { should have_link('delete', href: user_path(User.first)) }
-        it "should be able to delete another user" do
-          expect { click_link('delete') }.to change(User, :count).by(-1)
-        end
-        it { should_not have_link('delete', href: user_path(admin)) }
-      end
-    end
   end
-
-#  describe "index" do
-#    before do
-#      sign_in FactoryGirl.create(:user)
-#      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
-#      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
-#      visit users_path
-#    end
-#
-#    it { should have_selector('title', text: 'All users') }
-#    it { should have_selector('h1',    text: 'All users') }
-#
-#    it "should list each user" do
-#      User.all.each do |user|
-#        page.should have_selector('li', text: user.name)
-#      end
-#    end
-#  end
 
   describe "signup page" do
     before { visit signup_path }
@@ -162,4 +141,3 @@ describe "index" do
       specify { user.reload.email.should == new_email }
     end
   end
-end
